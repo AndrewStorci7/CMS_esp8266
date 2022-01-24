0301c20139aaa72202fe89e403a61e5eaa1520be
Andrew <75398539+AndrewStorci7@users.noreply.github.com>
2022-01-24 23:12:05 +0100

    Added default files

---
+++ php/config.php
@@ -0,0 +1,33 @@
+<?php
+/*$host = "127.0.0.1";
+$user_db = "root";
+$pw_db = "";
+$db_name = "as_proj_esp8266";
+
+$con = new mysqli($host, $user_db, $pw_db, $db_name);
+if($conn->connect_errno)
+    die("Connessione fallita: " . $conn->connect_errno);
+*/
+$config = [
+    'db_engine' => 'mysql',
+    'db_host' => '127.0.0.1',
+    'db_name' => 'as_proj_esp8266',
+    'db_user' => 'root',
+    'db_password' => '',
+];
+
+$db_config = $config['db_engine'] . ":host=".$config['db_host'] . ";dbname=" . $config['db_name'];
+
+try {
+    $pdo = new PDO($db_config, $config['db_user'], $config['db_password'], [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+    ]);
+        
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    exit("Impossibile connettersi al database: " . $e->getMessage());
+}
+
+//header("Location: ../index.php");
+?>
