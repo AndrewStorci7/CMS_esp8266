521714fe8952e3f70a6c87dc10832223ece4706b
Andrew <s.andrea.storci@gmail.com>
2022-01-28 19:35:36 +0100

    added canvas.js

--- php/select.php
+++ php/select.php
@@ -56,44 +56,9 @@ $risultato = $pdo->query($query);
 
 $index = 0;
 if($res->rowCount() > 0){
-    echo "<canvas id='myChart' width='400' height='400'></canvas>
-<script>
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Temperatura',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
-</script>";
+    echo "
+    <div class='container' style='width: 400 !important; height: 400 !important;'><canvas id='myChart'></canvas></div>
+    <script src='../js/canvas.js' type='text/javascript'></script>";
     echo '<div>
             <table>
               <tr>
