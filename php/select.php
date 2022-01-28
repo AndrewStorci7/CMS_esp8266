5f8871654cce41d80cd84c322ac609ab5b057494
Andrew <s.andrea.storci@gmail.com>
2022-01-28 17:56:46 +0100

    #Update select Updated the select.php, removed the funciton creation's test of a Matrix

--- php/select.php
+++ php/select.php
@@ -32,21 +32,20 @@ if($nomecercato!=''){
 
 
 $elementi_da_stampare = 10;
-$query = ('SELECT dati.temp, dispositivi.n_disp, utenti.nick, dati.data_time
+$query = 'SELECT dati.temp, dispositivi.n_disp, utenti.nick, dati.data_time
             FROM dati
             JOIN dispositivi
             JOIN utenti
-            ON dati.id_d = dispositivi.id_disp AND dispositivi.id_u = utenti.id ORDER BY dati.id DESC LIMIT ' . ($pagina-1) * $elementi_da_stampare . ',' . $elementi_da_stampare) . ';';
+            ON dati.id_d = dispositivi.id_disp AND dispositivi.id_u = utenti.id ORDER BY dati.id DESC LIMIT ' . ($pagina-1) * $elementi_da_stampare . ',' . $elementi_da_stampare . ';';
 $conta_elementi = 'SELECT COUNT(*) AS num_dati FROM dati';
 
-$num_elementi = $pdo->query($conta_elementi);
+$num_elementi = $pdo->prepare($conta_elementi);
 $num_elementi->execute();
 $riga = $num_elementi->fetch(PDO::FETCH_ASSOC);
 $num_pagine = $riga['num_dati'] / $elementi_da_stampare;
 
-$res = $pdo->query($query);
+$res = $pdo->prepare($query);
 $res->execute();
-$risultato = $res->fetch(PDO::FETCH_ASSOC);
 
 /*
 $num_elementi = $pdo->query($conta_elementi);
@@ -54,7 +53,69 @@ $riga = $num_elementi->fetch_assoc();
 $num_pagine = $riga['num_dati'] / $elementi_da_stampare;
 $risultato = $pdo->query($query);
 */
-if($risultato!== false && $risultato > 0){
+function arrayBidimensionale($param, $pdo){
+  $Matrice = $config = ['#', 'Temp', 'n_disp', 'nick', 'data_time'];
+  $query_eseg = $pdo->prepare($param);
+  $query_eseg->execute();
+
+  if($query_eseg){
+    return 0;
+  } else {
+    $index = 0;
+    while($fetch_query = $query_eseg->fetch(PDO::FETCH_ASSOC)) {
+      $Matrice = ['#' => strval($index++)];
+      $Matrice = ['Temp' => strval($fetch_query['temp'])];
+      $Matrice = ['#' => strval($fetch_query['n_disp'])];
+      $Matrice = ['#' => strval($fetch_query['nick'])];
+      $Matrice = ['#' => strval($fetch_query['data_time'])];
+    }
+  }
+  return $Matrice;
+}
+
+$index = 0;
+if($res->rowCount() > 0){
+    echo "<canvas id='myChart' width='400' height='400'></canvas>
+<script>
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Temperatura',
+            data: [12, 19, 3, 5, 2, 3],";
+
+
+            echo "
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+</script>";
     echo '<div>
             <table>
               <tr>
@@ -64,32 +125,40 @@ if($risultato!== false && $risultato > 0){
                 <th>Nickname User</th>
                 <th>Data e ora</th>
               </tr>';
-    $index = 0;
-    for($index; $index >= $risultato; $index++){        
+    $risultato_fun = arrayBidimensionale($query, $pdo);
+    $num_max = count($risultato_fun);
+    $num_max_y = count($risultato_fun, COUNT_RECURSIVE);
+    for($i = 0; $i < $num_max; $i++){
+      print_r($risultato_fun[$i]);
+      for($y = 0; $y < $num_max_y; $y++){
+        print_r($risultato_fun[$y]);
+      }
+    }
+    /*while($risultato = $res->fetch(PDO::FETCH_ASSOC)) {
+        $index++;
         $temp = $risultato['temp'];
         $n_disp = $risultato['n_disp'];
         $nick = $risultato['nick'];
         $data_time = $risultato['data_time'];
 
         echo '<tr>
-        <td>'. $index . '</td>
-        <td>'. $temp . '</td>
+        <td>' . $index . '</td>
+        <td>' . $temp . '</td>
         <td>' . $n_disp . '</td>
         <td>' . $nick . '</td>
         <td>' . $data_time . '</td>
         </tr>';
-    }
+    }*/
 echo '</table></div></div>';
 echo '<div class="pagine">';
 
-    if($num_pagine> $pagina){
-        echo '<p><center> <a class="piu" href="select.php?pagina='. ($pagina+1) . '">Pagina successiva '.($pagina+1).'>> </a> </center></p>';
+    if($num_pagine > $pagina){
+        echo '<br><p><center> <a class="piu" href="?pagina='. ($pagina+1) . '">Pagina successiva '.($pagina+1).'>> </a> </center></p>';
     }
 
     if($pagina > 1){
-       echo '<p> <center><a class="meno" href="select.php?pagina='. ($pagina-1) . '"> << '.($pagina-1).' Pagina precedente </a></center></p>';
+       echo '<br><p><center><a class="meno" href="?pagina='. ($pagina-1) . '"> << '.($pagina-1).' Pagina precedente </a></center></p>';
     }
 }
 echo '</div>';
 ?>
- ?>
