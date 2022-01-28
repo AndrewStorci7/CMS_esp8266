2ec6977fcbf9b54c36d2439a9f53dfc1c08dc6eb
Andrew <s.andrea.storci@gmail.com>
2022-01-28 17:57:54 +0100

    <h1>Titolo commit</h1> Prova descrizione

--- php/select.php
+++ php/select.php
@@ -53,25 +53,6 @@ $riga = $num_elementi->fetch_assoc();
 $num_pagine = $riga['num_dati'] / $elementi_da_stampare;
 $risultato = $pdo->query($query);
 */
-function arrayBidimensionale($param, $pdo){
-  $Matrice = $config = ['#', 'Temp', 'n_disp', 'nick', 'data_time'];
-  $query_eseg = $pdo->prepare($param);
-  $query_eseg->execute();
-
-  if($query_eseg){
-    return 0;
-  } else {
-    $index = 0;
-    while($fetch_query = $query_eseg->fetch(PDO::FETCH_ASSOC)) {
-      $Matrice = ['#' => strval($index++)];
-      $Matrice = ['Temp' => strval($fetch_query['temp'])];
-      $Matrice = ['#' => strval($fetch_query['n_disp'])];
-      $Matrice = ['#' => strval($fetch_query['nick'])];
-      $Matrice = ['#' => strval($fetch_query['data_time'])];
-    }
-  }
-  return $Matrice;
-}
 
 $index = 0;
 if($res->rowCount() > 0){
@@ -125,15 +106,7 @@ const myChart = new Chart(ctx, {
                 <th>Nickname User</th>
                 <th>Data e ora</th>
               </tr>';
-    $risultato_fun = arrayBidimensionale($query, $pdo);
-    $num_max = count($risultato_fun);
-    $num_max_y = count($risultato_fun, COUNT_RECURSIVE);
-    for($i = 0; $i < $num_max; $i++){
-      print_r($risultato_fun[$i]);
-      for($y = 0; $y < $num_max_y; $y++){
-        print_r($risultato_fun[$y]);
-      }
-    }
+
     /*while($risultato = $res->fetch(PDO::FETCH_ASSOC)) {
         $index++;
         $temp = $risultato['temp'];
