5296f8a54fd8c4e4d851cc115a66f927fa93c361
Andrew <s.andrea.storci@gmail.com>
2022-01-29 02:02:03 +0100

    Last update: 02:01 29-01-2022

--- php/API/api.php
+++ php/API/api.php
@@ -3,18 +3,11 @@ require_once('../config.php');
 
 date_default_timezone_set('Europe/Rome');
 $dataTime_invio = date('Y-m-d h:i:s', time());
-//echo $dataTime_invio;
 
 $temperatura = isset($_GET['temp']) ? $_GET['temp'] : '';
 $id_d = isset($_GET['id_d']) ? $_GET['id_d'] : '';
 
-//echo "<br>" . $temperatura . " " . $id_d;
-//$id_d_cast = intval($id_d);
-//$temp_cast = floatval($temperatura);
-//echo "<br>" . $temp_cast . " " . $id_d_cast;
-
 $insert_query = "INSERT INTO dati(temp, id_d, data_time) VALUES( :temp, :id_d, '$dataTime_invio')";
-//echo "<br>" . $insert_query;
 
 if(($temperatura == null || $temperatura == 'undefined' || $temperatura == "") || ($id_d == null || $id_d == 'undefined' || $id_d == "")){
     echo "<br>Campi vuoti";
@@ -24,11 +17,20 @@ if(($temperatura == null || $temperatura == 'undefined' || $temperatura == "") |
     $result->bindParam(':id_d', $id_d, PDO::PARAM_INT);
     $result->execute();
     //$matrice = $result->fetchAll(PDO::FETCH_DEFAULT);
-    $matrice = $result->fetchAll();
-    if($matrice > 0){
-        $json = json_encode($matrice);
-        $file = file_put_contents("data.json", $json);
-        //echo $file;
+    $risultato = $result->fetchAll();
+    if($risultato > 0){
+        $select_query = "SELECT * FROM dati WHERE id_d = :id_d AND temp = :temp";
+        $result = $pdo->prepare($insert_query);
+        $result->bindParam(':temp', $temperatura, PDO::PARAM_STR);
+        $result->bindParam(':id_d', $id_d, PDO::PARAM_INT);
+        $result->execute();
+
+        $matrice = $result->fetchAll();
+        if($matrice > 0){
+            $json = json_encode($matrice);
+            $file = file_put_contents("data.json", $json);
+        } else
+            echo 'non c\'è nulla da aggiungere';
     } else {
         echo "<br>Query non eseguita <br>";
     }
