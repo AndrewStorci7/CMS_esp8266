f35437d64858d6d67a92209d3ff01939649c6a6b
Andrew <s.andrea.storci@gmail.com>
2022-01-30 21:20:50 +0100

    Added alluserdata.php file, canvas_alldata.js

---
+++ js/canvas_alldata.js
@@ -0,0 +1,65 @@
+const ore = [];
+const temperature = [];
+var data_ora = new Date();
+getData();
+getChart();
+
+async function getChart(){
+  await getData();
+  const ctx = document.getElementById('myChart');
+  const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: ore,
+          datasets: [{
+              label: , //array conteneti i nomi delle persone o dei dispositivi
+              data: temperature,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+                ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)'
+              ],
+              borderWidth: 1,
+          },
+          {
+              label: , //array conteneti i nomi delle persone o dei dispositivi
+              data: temperature,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)'
+              ],
+              borderWidth: 1,
+          }
+        ]
+      },
+      options: {
+        scales: {
+              y: {
+                min: -50,
+                max: 50,
+              }
+            }
+      }
+  });
+}
+
+async function getData(){
+    const req = await fetch('../php/API/selectData.php');
+    const resp = await req.text();
+    JSON.stringify(resp);
+    const table = JSON.parse(resp);
+    //const tabella = table.split('\n').slice(4);
+    table.forEach(row => {
+        const hour = row['data_time'];
+        // il Data.parse lo userò per dividere la data dal orario
+        //ore.push(new Date(hour));
+        ore.push(hour);
+        const temp = row['temp'];
+        temperature.push(temp);
+        console.log(temp, hour);
+    });
+    console.log(temperature, ore);
+}
