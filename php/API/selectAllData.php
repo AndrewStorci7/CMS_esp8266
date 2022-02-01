1f68e9dfe4a864e3cab7deff1797d70084d4a77e
Andrew <s.andrea.storci@gmail.com>
2022-02-01 16:28:54 +0100

    Removed the for who slide the result of the query

--- php/API/selectAllData.php
+++ php/API/selectAllData.php
@@ -3,8 +3,7 @@ session_start();
 require_once('../config.php');
 
 if(isset($_SESSION['session_id'])){
-    $index = 1;
-    for($index; $index <= 22; $index++){
+  
       $select_query = "SELECT id_d, temp, data_time
                        FROM dati JOIN dispositivi
                        ON dati.id_d = dispositivi.id_disp
@@ -15,12 +14,11 @@ if(isset($_SESSION['session_id'])){
       $result2->execute();
       while($matrice = $result2->fetchAll(PDO::FETCH_DEFAULT)){
         if($matrice !== null){
-          $json = json_encode($matrice);
+          $json = json_encode("'" . $index . "': " . $matrice);
           echo $json;
           //$file = file_put_contents("data.json", $json);
         }
       }
-    }
 } else {
   echo "Non sei loggato";
 }
