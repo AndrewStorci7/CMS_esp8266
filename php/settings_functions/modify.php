a7584dab1acd9c5c251e2c1063893e93e8b9ffbc
Andrew <75398539+AndrewStorci7@users.noreply.github.com>
2022-02-03 13:26:36 +0100

    Updated modify.php

--- php/settings_functions/modify.php
+++ php/settings_functions/modify.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../config.php');
 $n_disp = isset($_POST['n_disp']) ? $_POST['n_disp'] : '';
-$id_disp = isset($_POST['id_disp']) ? $_POST['id_disp'] : '0';
+$id_disp = isset($_POST['id_disp']) ? $_POST['id_disp'] : '';
 $n_disp_cast = strval($n_disp);
 $id_disp_cast = intval($id_disp);
 $modify_query = "UPDATE dispositivi
