fd2303607e8e2f8052d477b40e80e599d70e2d58
Andrew <s.andrea.storci@gmail.com>
2022-02-18 18:22:55 +0100

    Mi sposto con il pc

--- js/canvas_alldata.js
+++ js/canvas_alldata.js
@@ -53,16 +53,15 @@ async function getData(){
     const table = JSON.parse(resp);
     const tabella = new Array(table);
     //console.log(tabella);
-
     // da finire, forse da cambiare proprio logica,
     // cercare di utilizzare i dataset
-    for(let i = 0; i < tabella.length; i++){
-        var primo_array = tabella[i];
-        console.log(primo_array);
-        const temp = primo_array['temp'];
-        temperature.push(temp);
-        console.log(temp);
-        const data_time = primo_array['data_time'];
-        ore.push(data_time);
+    for(var i = 0; i < tabella.length; i++){
+        var id_d = [];
+        id_d = tabella[i];
+        console.log(id_d);
+        for(var y = 0; y < id_d.length; y++){
+          console.log("sono entrato");
+        }
+
     }
 }
--- php/API/selectAllData.php
+++ php/API/selectAllData.php
@@ -2,7 +2,7 @@
 session_start();
 require_once('../config.php');
 
-if(isset($_SESSION['session_id'])){
+if(isset($_SESSION['session_id']) && (isset($_SESSION['session_role']) && $_SESSION['session_role'] == 1)){
       $select_query = "SELECT id_d, temp, data_time
                        FROM dati JOIN dispositivi
                        ON dati.id_d = dispositivi.id_disp
@@ -10,15 +10,15 @@ if(isset($_SESSION['session_id'])){
 
       $result2 = $pdo->prepare($select_query);
       $result2->execute();
+      //$array = array();
       while($matrice = $result2->fetchAll(PDO::FETCH_GROUP)){
         if($matrice !== null){
           $json = json_encode($matrice);
-          echo $json;
-          //$file = file_put_contents("data.json", $json);
         }
       }
+      echo $json;
 } else {
-  echo "Non sei loggato";
+  echo "Non sei loggato o non sei un amministratore";
 }
 
 
